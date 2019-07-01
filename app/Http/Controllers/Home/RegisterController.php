990c01979bfc88ee4c75c99e06b02ac9d98530da
wwx <kalaaimeng@163.com>
2019-07-01 11:56:27 +0800

    190701wwx;

--- app/Http/Controllers/Home/RegisterController.php
+++ app/Http/Controllers/Home/RegisterController.php
@@ -66,11 +66,16 @@ class RegisterController extends Controller
                 // to 发送到的地址 subject 邮件标题
                 $s = $m->to($email)->subject('【奈斯商城】 提醒邮件');
                  if($s){
-                     echo "<script>alert('用户注册成功,请尽快完成激活');location.href='https://mail.qq.com/cgi-bin/loginpage'</script>";
+                     $this->status =  1;
+                     echo "<script>alert('用户注册成功,请尽快完成激活')</script>";
                     }
+
                 });
             }else{
                 echo "<script>alert('注册失败');location.href='/home/register'</script>";
+            }
+            if($this->status == 1){
+               return view('home.register.mail',['id' => $user->id,'token'=>$token]);  
             }
         }else{
             echo "<script>alert('用户已存在');location.href='/home/register'</script>";
